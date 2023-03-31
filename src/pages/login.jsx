416b2363cab016a4086ea93142dbd5cadcca6932
wildchamo <wildchamo@gmail.com>
2023-03-30 21:15:08 -0500

    importación imagen y base del proyecto

---
+++ src/pages/login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Stack from '@mui/material/Stack';
+import loginImg from "../img/loginimg.png";
+
+
+function Login() {
+  return (
+    <Stack>
+       <img style={{height: "100vh"}} src={loginImg}/>
+       
+    </Stack>
+  )
+}
+
+export default Login
