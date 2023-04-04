769190bec00f2e1f76f76117fbe207ce8396845d
wildchamo <wildchamo@gmail.com>
2023-04-03 21:17:18 -0500

    instalación react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import { createMuiTheme, ThemeProvider } from "@mui/material/styles";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+const theme = createMuiTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#9e0b0f",
+    },
+    secondary: {
+      main: "#FAFAFB",
+      contrastText: "#9E0B0F",
+      dark: "#9e0b0f",
+    },
+  },
+});
 
 function App() {
-  return <div>
-    <Login/>
-    
-  </div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </HashRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
