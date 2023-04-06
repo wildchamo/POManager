247ef040799bb8054437e2d99081449bf4b34da2
wildchamo <wildchamo@gmail.com>
2023-04-05 22:55:07 -0500

    mejoras a la maquetación con grid

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,62 +5,84 @@ import {
   IconButton,
   InputAdornment,
   Stack,
+  Grid,
 } from "@mui/material";
 import React from "react";
 import logoHeader from "../img/logoheader.png";
-
+import Container from "@mui/material/Container";
 import LogoutIcon from "@mui/icons-material/Logout";
 import SearchIcon from "@mui/icons-material/Search";
 
 function Header({ username }) {
   return (
-    <AppBar
+    <Grid
+      container
       sx={{
-        backgroundColor: "white",
-        color: "#C4161C",
-        boxShadow: "none",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-around",
-        paddingTop: "10px",
+        position: "fixed",
+        zIndex: "1100",
+        top: "0",
+        left: "auto",
+        marginTop: "14px",
+        marginLeft: "10px",
       }}
     >
-      <Stack
-        sx={{
+      <Grid item xs={5}>
+        <Stack
+          sx={{
             flexDirection: "row",
             alignItems: "center",
-            justifyContent: "space-between",
+            justifyContent: "space-around",
+          }}
+        >
+          <img src={logoHeader} />
+          <Typography variant="h5" sx={{ fontWeight: "700" }}>
+            ¡Bienvenido a OP manager, {username}!
+          </Typography>
+        </Stack>
+      </Grid>
+
+      <Grid
+        item
+        xs={6}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
         }}
       >
-        <img src={logoHeader} />
-        <Typography variant="h5" sx={{ fontWeight: "700" }}>
-          ¡Bienvenido a OP manager, {username}!
-        </Typography>
-      </Stack>
+        <TextField
+          item
+          id="outlined-basic"
+          label="Buscar proyecto"
+          placeholder="Introduce el nombre del proyecto que buscas"
+          sx={{
+            width: "500px",
+          }}
+          // value={valueInput}
+          // onChange={onSearchValueChange} onClick={onHandleSubmit}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton>
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
 
-      <TextField
-        id="outlined-basic"
-        label="Buscar proyecto"
-        placeholder="Introduce el nombre del proyecto que buscas"
+      <Grid
+        item
+        xs={1}
         sx={{
-          width: "500px",
+          alignItems: "center",
         }}
-        // value={valueInput}
-        // onChange={onSearchValueChange} onClick={onHandleSubmit}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton>
-                <SearchIcon />
-              </IconButton>
-            </InputAdornment>
-          ),
-        }}
-      />
-      <IconButton>
-        <LogoutIcon />
-      </IconButton>
-    </AppBar>
+      >
+        <IconButton item>
+          <LogoutIcon />
+        </IconButton>
+      </Grid>
+    </Grid>
   );
 }
 
