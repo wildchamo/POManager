e256f4a749165d10a2551bd8e93d098fb6770195
wildchamo <wildchamo@gmail.com>
2023-04-06 10:52:12 -0500

    creación del componente datatable para desplegar el dashboard

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,13 +1,36 @@
 import { Paper, Typography } from "@mui/material";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+
 import React from "react";
 
-function DataTable({unidad}) {
+function DataTable({ unidad }) {
   return (
     <Paper elevation={1}>
-      <Typography variant="h6" color="initial" sx={{textAlign:"center"}}>
+      <Typography variant="h6" color="initial" sx={{ textAlign: "center" }}>
         Proyectos operativos de la Unidad de {unidad}{" "}
       </Typography>
 
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align="center" sx={{width:""}}>Nombre</TableCell>
+            <TableCell align="center">Fecha de inicio</TableCell>
+            <TableCell align="center">Fecha de finalización</TableCell>
+            <TableCell align="center">Estado</TableCell>
+            <TableCell align="center">Editar</TableCell>
+            <TableCell align="center">Ver más</TableCell>
+            <TableCell align="center">Anular</TableCell>
+          </TableRow>
+        <TableBody>
+            
+        </TableBody>
+
+
+        </TableHead>
+      </Table>
     </Paper>
   );
 }
