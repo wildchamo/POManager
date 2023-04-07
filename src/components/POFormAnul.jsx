aadf1d11da482f9785662c9af7a6102f7169a3d8
wildchamo <wildchamo@gmail.com>
2023-04-07 17:27:30 -0500

    base maquetación con mui para formulario de cancelación

--- src/components/POFormAnul.jsx
+++ src/components/POFormAnul.jsx
@@ -5,24 +5,50 @@ function POFormAnul({ proyecto }) {
   return (
     <Grid
       component="form"
-    //   onSubmit={handleClick}
+      //   onSubmit={handleClick}
       container
       sx={{
         position: "absolute",
         top: "50%",
         left: "50%",
         transform: "translate(-50%, -50%)",
-        width: "80%",
-        height: "60%",
+        width: "60%",
+        height: "40%",
         bgcolor: "background.paper",
         border: "2px solid #000",
         boxShadow: 24,
         p: 4,
       }}
     >
-      <Typography variant="h1" color="initial">
-        Hola, soyJose
-      </Typography>
+      <Grid item xs={12}>
+        <Typography variant="h5" color="initial">
+          Por qué desea anular el proyecto {proyecto.nombre}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          //   value={nombre}
+          //   onChange={(e) => setNombre(e.target.value)}
+          margin="normal"
+          id="outlined-basic"
+          label="Justificación"
+          placeholder="Ingresa la justificación :("
+          autoFocus
+          fullWidth
+          required
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Stack
+          direction="row"
+          justifyContent="flex-end"
+          alignItems="center"
+          spacing={4}
+        >
+          <Button variant="contained">Cancelar</Button>
+          <Button>Anular proyecto</Button>
+        </Stack>
+      </Grid>
     </Grid>
   );
 }
