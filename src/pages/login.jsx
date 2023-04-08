a11bf9e6cc336960b3de012990cfeb3685d1d47a
wildchamo <wildchamo@gmail.com>
2023-04-08 12:33:25 -0500

    display se muestra en md pero  no en xs

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -7,10 +7,10 @@ import { Grid } from "@mui/material";
 function Login() {
   return (
     <Grid container direction="row">
-      <Grid item md={5}>
+      <Grid item md={5} xs={12}>
         <LoginBar />
       </Grid>
-      <Grid item md={7}>
+      <Grid item md={7} sx={{ display: { xs: 'none', sm: 'block' } }}>
         <img src={loginImg} />
       </Grid>
     </Grid>
