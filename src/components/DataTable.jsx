7124b6807e35a24e9bbd214f9d0d3c044c4f8cec
wildchamo <wildchamo@gmail.com>
2023-04-07 22:27:44 -0500

    fake pagination

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -9,6 +9,8 @@ import POproyect from "./POproyect";
 import Modal from "@mui/material/Modal";
 import POFormCreate from "./POFormCreate";
 import { useAuth } from "../context";
+import TablePagination from "@mui/material/TablePagination";
+import { Stack } from "@mui/material";
 
 import React from "react";
 
@@ -18,6 +20,17 @@ function DataTable({ unidad, proyectos }) {
   const handleClose = () => setOpen(false);
 
   const auth = useAuth();
+  const [page, setPage] = React.useState(1);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 5));
+    setPage(0);
+  };
 
   return (
     <Paper elevation={1} sx={{ paddingTop: "20px" }}>
@@ -79,40 +92,53 @@ function DataTable({ unidad, proyectos }) {
           {proyectos.map((proyecto) => (
             <POproyect key={proyecto.id} proyecto={proyecto} />
           ))}
-
-          <TableRow>
-            <Button
-              onClick={handleOpen}
-              variant="contained"
-              startIcon={<AddIcon />}
-            >
-              Crear nuevo proyecto
-            </Button>
-            <Modal open={open} onClose={handleClose}>
-              <POFormCreate
-                closeModal={handleClose}
-                handleEvent={(
-                  nombre,
-                  estado,
-                  fechaInicio,
-                  fechaFin,
-                  fechaCreacion,
-                  descripcion
-                ) =>
-                  auth.createPO(
-                    nombre,
-                    estado,
-                    fechaInicio,
-                    fechaFin,
-                    fechaCreacion,
-                    descripcion
-                  )
-                }
-              />
-            </Modal>
-          </TableRow>
         </TableBody>
       </Table>
+      <Stack
+        direction="row"
+        justifyContent="space-around"
+        alignItems="center"
+        spacing={2}
+      >
+        <TablePagination
+          component="div"
+          count={auth.noProyectos}
+          page={page}
+          onPageChange={handleChangePage}
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+        <Button
+          onClick={handleOpen}
+          variant="contained"
+          startIcon={<AddIcon />}
+        >
+          Crear nuevo proyecto
+        </Button>
+      </Stack>
+
+      <Modal open={open} onClose={handleClose}>
+        <POFormCreate
+          closeModal={handleClose}
+          handleEvent={(
+            nombre,
+            estado,
+            fechaInicio,
+            fechaFin,
+            fechaCreacion,
+            descripcion
+          ) =>
+            auth.createPO(
+              nombre,
+              estado,
+              fechaInicio,
+              fechaFin,
+              fechaCreacion,
+              descripcion
+            )
+          }
+        />
+      </Modal>
     </Paper>
   );
 }
